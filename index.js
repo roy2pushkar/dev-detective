bcb20cc7d32f8026bc035295efd0a7418124bf53
roy2pushkar <roy2000pushkar@gmail.com>
2023-04-08 21:07:19 +0530

    Dev-Detective

---
+++ index.js
@@ -0,0 +1,10 @@
+//Variables
+const searchbar = document.querySelector(".searchbar-container");
+const profilecontainer = document.querySelector(".profile-container");
+const root = document.documentElement.style;
+const get = (param) => document.getElementById(`${param}`);
+const url = "https://api.github.com/users/";
+const noresults = get("no-results");
+const btnmode = get("btn-mode");
+const modetext = get("mode-text");
+const modeicon = get("mode-icon");
