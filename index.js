23a699b64d143df50494b653fc8b690890614057
roy2pushkar <roy2000pushkar@gmail.com>
2023-04-29 23:55:14 +0530

    Dev Detective

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ const noresults = get("no-results");
 const btnmode = get("btn-mode");
 const modetext = get("mode-text");
 const modeicon = get("mode-icon");
+const btnsubmit = get("submit");
+const input = get("input");
+const avatar = get("avatar");
+const userName = get("name");
+const user = get("user");
+const date = get("date");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const bio = get("bio");
+const repos = get("repos");
+const followers = get("followers");
+const following = get("following");
+const user_location = get("location");
+const page = get("page");
+const twitter = get("twitter");
+const company = get("company");
+let darkMode = false;
