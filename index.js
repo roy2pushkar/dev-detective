c98541fd685d3ed964814fd055497870ab5a693c
roy2pushkar <roy2000pushkar@gmail.com>
2023-05-15 20:14:08 +0530

    Dev Detective

--- index.js
+++ index.js
@@ -81,3 +81,18 @@ function getUserData(gitUrl) {
       throw error;
     });
 }
+
+function updateProfile(data) {
+  if (data.message !== "Not found") {
+    noresults.style.display = "none";
+    function checkNull(param1, param2) {
+      if (param1 === "" || param1 === null) {
+        param2.style.opacity = 0.5;
+        param2.previousElementSibling.style.opacity = 0.5;
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+}
